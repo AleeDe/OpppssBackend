3a6424f726cdbc18db39ec6e2df7c264c919e994
Muhammad Ali <122118041+AleeDe@users.noreply.github.com>
2024-12-14 23:51:57 +0500

    Update WebConfig.java

--- src/main/java/com/aleedev/SecuityCheck/Config/WebConfig.java
+++ src/main/java/com/aleedev/SecuityCheck/Config/WebConfig.java
@@ -14,6 +14,7 @@ public class WebConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://44.223.145.60:3000");
         config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
